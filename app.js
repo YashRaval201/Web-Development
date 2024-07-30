ce8374ba18b775b474959fc6f057dd7fe3f2c00c
Yash Raval <yashraval201@gmail.com>
2024-07-31 02:15:02 +0530

    Level 4 Authentication using Bcrypt

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 import dotenv from "dotenv"
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose"
-// import encrypt from "mongoose-encryption"
-import md5 from "md5";
+import mongoose from "mongoose";
+// import encrypt from "mongoose-encryption";
+// import md5 from "md5";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
 
 dotenv.config(); 
 
@@ -17,7 +20,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true})
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true})
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.error("Could not connect to MongoDB", err));
 
@@ -47,31 +50,36 @@ app.get("/logout", function(req, res) {
 })
 
 app.post("/register",async function(req, res) {
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        });
+        try {
+            await newUser.save();+
+            res.render("secrets");
+        } catch (err) {
+            console.log(err);
+        }
     });
-    try {
-        await newUser.save();
-        res.render("secrets");
-    } catch (err) {
-        console.log(err);
-    }
 })
 
 app.post("/login", async function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
+    // const password = md5(req.body.password);
 
     try{
         const foundUser = await User.findOne({ email: username });
         if(foundUser) {
-            if(foundUser.password === password) {
-                res.render("secrets");
-            }
-            else {
-                res.send("Incorrect Password");
-            }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result === true) {
+                    res.render("secrets");
+                }
+                else {
+                    res.send("Incorrect Password");
+                }
+            });
         } else {
             res.send("User not found");
         }
