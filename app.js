60c8496db8117463c1085e39ff9c4ec428e3afe7
Yash Raval <yashraval201@gmail.com>
2024-07-30 21:36:13 +0530

    Level 3 Encryption Using md5

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv"
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose"
-import encrypt from "mongoose-encryption"
+// import encrypt from "mongoose-encryption"
+import md5 from "md5";
 
 dotenv.config(); 
 
@@ -25,7 +26,7 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,10 +42,14 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
+app.get("/logout", function(req, res) {
+    res.render("home");
+})
+
 app.post("/register",async function(req, res) {
     const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     try {
         await newUser.save();
@@ -56,7 +61,7 @@ app.post("/register",async function(req, res) {
 
 app.post("/login", async function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     try{
         const foundUser = await User.findOne({ email: username });
